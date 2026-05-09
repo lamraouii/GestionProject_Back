18571d9cf64c651a1c5324c66e56a2b1167741e0
sousou <ismail.lamraoui123@gmail.com>
2026-05-09 19:00:54 +0100

    initialize the project

---
+++ src/main/java/com/ensao/gestionprojet/GestionprojetApplication.java
@@ -0,0 +1,14 @@
+package com.ensao.gestionprojet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionprojetApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionprojetApplication.class, args);
+	}
+
+}
